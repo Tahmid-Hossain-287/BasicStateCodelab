b13e9ce21a9dbe9bf6a455665bee5087f24a6f3b
tahmid <mstahmid0@gmail.com>
2024-09-25 14:16:09 -0500

    everything before introducing viewmodel

--- app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
+++ app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
@@ -1,10 +1,21 @@
 package com.example.basicstatecodelab
 
-import WaterCounter
+import StatefulCounter
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    WaterCounter(modifier)
+    Column(modifier = modifier) {
+        StatefulCounter()
+
+        fun getWellnessTasks() = List(30) {i -> WellnessTask(i, "Task # $i") }
+        val list = remember { getWellnessTasks().toMutableStateList() }
+        WellnessTasksList(list = list, onCloseTask = { task -> list.remove(task) })
+    }
 }
+
+private fun getWellnessTasks() = List(30) {i -> WellnessTask(i, "Task # $i") }
--- app/src/main/java/com/example/basicstatecodelab/WellnessTaskItem.kt
+++ app/src/main/java/com/example/basicstatecodelab/WellnessTaskItem.kt
@@ -15,6 +15,7 @@ import androidx.compose.material3.Checkbox
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 
 @Composable
@@ -45,12 +46,14 @@ fun WellnessTaskItem(
 }
 
 @Composable
-fun WellnessTaskItem(taskName: String, modifier: Modifier = Modifier) {
-    var checkedState by remember { mutableStateOf(false) }
+fun WellnessTaskItem(
+    taskName: String, onClose: () -> Unit, modifier: Modifier = Modifier
+) {
+    var checkedState by rememberSaveable { mutableStateOf(false) }
 
     WellnessTaskItem(
         taskName = taskName,
-        onClose = { }, // this will be implemented later
+        onClose = onClose,
         checked = checkedState,
         onCheckedChange = { newValue -> checkedState = newValue },
         modifier = modifier
